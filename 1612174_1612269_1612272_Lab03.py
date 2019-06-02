cc92676496323cefb37be9f113fc2889edff00b9
dantevergilkat <dantevergilkat@gmail.com>
2019-06-02 13:12:24 +0700

    Init

--- 1612174_1612269_1612272_Lab03.py
+++ 1612174_1612269_1612272_Lab03.py
@@ -87,7 +87,8 @@ class CPlateDetection:
             bounding_rect = cv2.minAreaRect(contours[i])
             # Validate bounding_rect base on its area and aspect ratio
             if(not(self.verify_sizes(bounding_rect))):
-                contours.pop(i)
+                """??? VARIABLE BY REFERENCE ???"""
+                contours.pop(i) 
                 continue
             
             # Push this bounding rect into rect list
@@ -112,7 +113,6 @@ class CPlateDetection:
             minSize = minSize - minSize*0.5
     """
 
-    # Process plate image with some stuffs: dilate, threshold, contours and verify size to refine plate image
     def process_plate_image(self, plateImg):
         # Convert to Gray-Scale
         grayPlate = cv2.cvtColor(plateImg, cv2.COLOR_BGR2GRAY)
@@ -126,6 +126,9 @@ class CPlateDetection:
         #ret, threshImg = cv2.threshold(dilateImg, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         # Find contours
         contours, hierachy = cv2.findContours(np.copy(threshImg), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # get corner points on boundary
+        tmpImg = plateImg.copy()
+        cv2.drawContours(tmpImg,contours,-1,(0,255,0),3)
+        cv2.imshow('Contours',tmpImg)
 
         # If contours exists
         if len(contours) > 0:
@@ -137,16 +140,129 @@ class CPlateDetection:
             
             # Get bouding rect of this contour
             x, y, w, h = cv2.boundingRect(contours[imax])
-
             # Verify plate image again
             if not(self.verify_sizes([(x, y), (w, h)])):
                 return None, None
             
             # Crop region that have number in image
             found_plate = np.copy(plateImg[y:y+h, x:x+w])
+            #cv2.imshow('Found plate',found_plate)
             return found_plate, [x, y, w, h]
-        
         return None, None
+
+    # checking the posibility of being character based on image's area
+    def verify_size_character(self, img):
+        #grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        threshImg = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,7,5)
+        aspect = float(45/77)
+        height,width = threshImg.shape
+        charAspect = float(width/height)
+        error = 0.35
+        minHeight, maxHeight = 30, 56
+        minAspect, maxAspect = 0.2, aspect + aspect*error
+
+        area = cv2.countNonZero(threshImg)
+        bbArea = width * height
+
+        percPixels = float(area/bbArea)
+        if percPixels < 0.8 and charAspect > minAspect and charAspect < maxAspect \
+            and height >= minHeight and height < maxHeight:
+            return True
+        return False
+
+
+    # Separating each character in plate image
+    def get_character_images(self, plateImg):
+    #def process_plate_image(self,plateImg):
+        ret = [] # list of character images
+        # Convert to Gray-Scale
+        height,width = plateImg.shape[:2]
+        plateImg = cv2.resize(plateImg,(2*width,2*height))
+        plateImg = cv2.GaussianBlur(plateImg,(3,3),1.2)
+        #plateImg[:,:] = int(plateImg[:,:] * 1.2)
+
+        grayPlate = cv2.cvtColor(plateImg, cv2.COLOR_BGR2GRAY)
+        grayPlate = cv2.copyMakeBorder(grayPlate,2,2,2,2,cv2.BORDER_REPLICATE)
+        plateImg = cv2.copyMakeBorder(plateImg,2,2,2,2,cv2.BORDER_REPLICATE)
+        height, width = grayPlate.shape
+        cv2.imshow('grayPlate',grayPlate)
+        # Blur image
+        # grayPlate = cv2.GaussianBlur(grayPlate, (5, 5), 1.0) # remove noise
+        #_, threshImg = cv2.threshold(grayPlate, 80, 255, cv2.THRESH_BINARY_INV)
+        threshImg = cv2.adaptiveThreshold(grayPlate,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,7,2)
+        cv2.imshow('Thresh',threshImg)
+
+        # Opening for removing noise
+        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (2,2))
+        openImg = cv2.morphologyEx(threshImg,cv2.MORPH_OPEN,kernel)
+        cv2.imshow('Open',openImg)
+
+        # Erode plate image for removing noise
+        '''kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (2,2))
+        erodeImg = cv2.erode(threshImg, kernel, iterations = 1)
+        cv2.imshow('Erode', erodeImg)'''
+
+        # Dilate plate image for OCR
+        '''kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (2, 2))
+        dilateImg = cv2.dilate(openImg, kernel, iterations = 1)
+        cv2.imshow('Dilate',dilateImg)'''
+        # cv2.imshow('Dilate',dilateImg)
+        # Threshold image
+        #_, threshImg = cv2.threshold(dilateImg, 150, 255, cv2.THRESH_BINARY)
+        #cv2.imshow('Thresh',threshImg)
+        # cv2.imshow('Threshold',threshImg)
+        #ret, threshImg = cv2.threshold(dilateImg, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # Find contours
+        
+        contours, hierachy = cv2.findContours(np.copy(openImg), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # get corner points on boundary
+        
+        tmpImg = plateImg.copy()
+        cv2.drawContours(tmpImg,contours,-1,(0,255,0),1)
+        cv2.imshow('Char Contours',tmpImg)
+        cv2.imwrite('con.jpg',tmpImg)
+        # If contours exists
+        print('Number of contours: ', len(contours))
+        if len(contours) > 7:
+            # Compute Area of each contour (Moment M[00])
+            area_list = [cv2.contourArea(cnt) for cnt in contours]
+            
+            index = []
+            x,y,w,h = (0,0,0,0)
+            for (i,cnt) in enumerate(contours):
+                # Get bouding rect of this contour
+                x, y, w, h = cv2.boundingRect(cnt)
+                # Verify sub image
+                '''if i != 9:
+                    continue'''
+                # Crop region that have number in image
+                #found_plate = np.copy(grayPlate[y:y+h, x:x+w])
+                
+                if y - 2 >= 0 and x - 2 >= 0 and y+h+2 < height and x+w+2 < width:
+                    found_plate = np.copy(grayPlate[y - 2:y + h + 2, x - 2:x + w + 2])
+                else:
+                    found_plate = np.copy(grayPlate[y:y + h, x:x + w])
+                
+                # found_plate = cv2.copyMakeBorder(found_plate,2,2,2,2,cv2.BORDER_CONSTANT,value=[255,255,255])
+                #print('Check size')
+                '''tmpImg = plateImg.copy()
+                cv2.drawContours(tmpImg,contours,i,(0,255,0),1)
+                cv2.imshow('Char Contour' + str(i),tmpImg)'''
+                if self.verify_size_character(found_plate):
+                    #print(str(i))
+                    '''tmpImg = plateImg.copy()
+                    cv2.drawContours(tmpImg,contours,i,(0,255,0),1)
+                    cv2.imshow('Char Contour' + str(i),tmpImg)'''
+                    #cv2.imshow('Char' + str(i), found_plate)
+                    index.append(x)
+                    ret.append((found_plate,(x,y,w,h)))
+
+
+            indices = np.argsort(index)
+            ret = [x for _,x in sorted(zip(index,ret))]
+            
+            return ret
+        
+        return None
 
     def makeBoundingBoxOfCandidateContours(self, candidate_cnts):
         boundingBox = []
@@ -268,7 +384,9 @@ class CPlateDetection:
         # Sort contour base on area (Descending order)
         contours = sorted(contours, key = cv2.contourArea, reverse = True)
 
-        for cnt in contours:
+        for (i,cnt) in enumerate(contours):
+            '''if i == 1:
+                break'''
             x, y, w, h = cv2.boundingRect(cnt)
             plateImg = np.copy(img[y:y+h, x:x+w])
 
@@ -278,11 +396,12 @@ class CPlateDetection:
                 plt.title("Possible plates")
                 plt.axis("off")
 
+            cv2.imshow('Detected Plate' + str(i), plateImg)
             # Process image with some stuffs: dilate, threshold, contours and verify size to refine plate image
             found_plate, plat_rect = self.process_plate_image(plateImg)
             if found_plate is None:
                 continue
-            
+            #cv2.imshow('Found plate', found_plate)
             # Take advantage of white background, so we will check if the average color >= 100, it will a plate
             if np.mean(found_plate).astype(np.uint8) >= 100:
                 # Display plate
@@ -291,23 +410,72 @@ class CPlateDetection:
                 plt.title("Correct plates")
                 plt.axis("off")
 
+                plateNumber = ""
+                charImgs = self.get_character_images(found_plate)
+                if charImgs is None:
+                    continue
+                #print(len(charImgs))
+                config = ("-l eng --oem 1 --psm 7")
+                old_x,old_y,old_w,old_h = -1,-1,-1,-1
+                for (i,charInf) in enumerate(charImgs):
+                    #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+                    #closingImg = cv2.morphologyEx(charImg, cv2.MORPH_CLOSE, kernel)
+                    x,y,w,h = charInf[1]
+                    if x - old_x < 8:
+                        continue
+                    c_height, c_width = charInf[0].shape
+                    #charImg = cv2.resize(charImg, (c_width*2,int(c_height*1.5)))
+                    
+
+                    charImg = cv2.copyMakeBorder(charInf[0],2,2,2,2,cv2.BORDER_REPLICATE)
+                    charImg = cv2.adaptiveThreshold(charImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,13,5)
+                    charImg = cv2.GaussianBlur(charImg,(3,3),1.5)
+                    '''cv2.imshow('new size: ' + str(i),charImg)
+
+                    threshPlate = cv2.adaptiveThreshold(charImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,13,5)
+                    cv2.imshow('Thresh: '+ str(i), threshPlate)
+
+                    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
+                    openImg = cv2.morphologyEx(threshPlate,cv2.MORPH_OPEN,kernel)
+
+
+                    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+                    #eroseImg = cv2.erode(threshPlate, kernel, iterations = 1)
+                    dilateImg = cv2.dilate(openImg, kernel, iterations = 1)'''
+                    
+                    old_x,old_y,old_w,old_h = x,y,w,h
+
+                    #cv2.imshow('Thresh char' + str(i), threshPlate)
+                    cv2.imshow('Dilate char' + str(i), charImg)
+                    char = image_to_string(charImg, config = config)
+                    char = char.replace(" ", "")
+                    print(char,' ',x)
+                    plateNumber += char
+                print(plateNumber)
                 # ----------------------Now we use Tesseract to recognize charactor in plate-----------------------
                 # Convert to gray image
-                grayPlate = cv2.cvtColor(found_plate, cv2.COLOR_BGR2GRAY)
+                '''grayPlate = cv2.cvtColor(found_plate, cv2.COLOR_BGR2GRAY)
+                height,width = grayPlate.shape
+                # increasing size of the image
+                grayPlate = cv2.resize(grayPlate,(2*width,2*height))
+                cv2.imshow('Gray', grayPlate)
                 # Blur image
-                blurPlate = cv2.GaussianBlur(grayImg, (5, 5), 1.0) # Remove noise
+                blurPlate = cv2.GaussianBlur(grayPlate, (5, 5), 1.2) # Remove noise
                 # Apply closing operation:  Dilation followed by Erosion to remove noise and hole inside object
-                kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+                kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
                 closingImg = cv2.morphologyEx(blurPlate, cv2.MORPH_CLOSE, kernel)
-
+                cv2.imshow('morpho',closingImg)
                 # Apply threshold to get image with only b&w (binarization)
-                _, threshPlate = cv2.threshold(closingImg, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
+                #_, threshPlate = cv2.threshold(closingImg, 120, 255, cv2.THRESH_BINARY) #+ cv2.THRESH_OTSU)
+                threshPlate = cv2.adaptiveThreshold(closingImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,7,5)
+                #v2.imshow('Thresh',threshPlate)
                 # Store image in PIL image format
                 pilImg = Image.fromarray(threshPlate)
-
+                pilImg = np.array(pilImg)
+                cv2.imshow('pilImg',pilImg)
+                cv2.imwrite('test1.jpg',pilImg)
                 # Recognize text with Tesseract
-                plate_text = image_to_string(found_plate, lang = "eng")
+                plate_text = image_to_string(pilImg, lang = "eng")
                 plate_text = plate_text.replace(" ", "") # Remove space
 
                 if len(plate_text) == 0:
@@ -325,10 +493,10 @@ class CPlateDetection:
                 plt.figure()
                 plt.imshow(cv2.cvtColor(copy_img, cv2.COLOR_BGR2RGB))
                 plt.title("Plate number recognition")
-                plt.axis("off")
+                plt.axis("off")'''
 
-                print(plate_text)
-                return plate_text
+                #print(plate_text)
+                return plateNumber
 
         print("None")
         return "None"
@@ -374,7 +542,8 @@ def main(args):
     time = (e2 - e1)/cv2.getTickFrequency()
     print('Time: %.2f(s)' %(time))
 
-    plt.show()
+    #plt.show()
+    cv2.waitKey()
 
 if __name__ == "__main__":
     main(args)
---
+++ Files_separation/chars_recognition.py
@@ -0,0 +1,124 @@
+import numpy as np
+import cv2
+from pytesseract import image_to_string
+import matplotlib.pyplot as plt
+import chars_separation
+
+class CharactersRecognition:
+
+    def __init__(self):
+        self.init = 0
+
+
+    def recognize_characters(self, listOfPlates):
+        for (i, found_plate) in enumerate(listOfPlates):
+            '''if i == 1:
+                break'''
+            
+            #cv2.imshow('Found plate', found_plate)
+            # Take advantage of white background, so we will check if the average color >= 100, it will a plate
+            if np.mean(found_plate).astype(np.uint8) >= 100:
+                # Display plate
+                plt.figure()
+                plt.imshow(cv2.cvtColor(found_plate, cv2.COLOR_BGR2RGB))
+                plt.title("Correct plates")
+                plt.axis("off")
+
+                plateNumber = ""
+
+                charsSeparator = chars_separation.CharactersSeparation()
+                charImgs = charsSeparator.get_character_images(found_plate)
+                if charImgs is None:
+                    continue
+                #print(len(charImgs))
+                config = ("-l eng --oem 1 --psm 7")
+                old_x,old_y,old_w,old_h = -1,-1,-1,-1 # for solving "O" or "o" or "0", which have 2 contours
+                for (i,charInf) in enumerate(charImgs):
+                    #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+                    #closingImg = cv2.morphologyEx(charImg, cv2.MORPH_CLOSE, kernel)
+                    x,y,w,h = charInf[1]
+                    if x - old_x < 8:
+                        continue
+                    c_height, c_width = charInf[0].shape
+                    #charImg = cv2.resize(charImg, (c_width*2,int(c_height*1.5)))
+                    
+
+                    charImg = cv2.copyMakeBorder(charInf[0],2,2,2,2,cv2.BORDER_REPLICATE)
+                    charImg = cv2.adaptiveThreshold(charImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,13,5)
+                    charImg = cv2.GaussianBlur(charImg,(3,3),1.5)
+                    '''cv2.imshow('new size: ' + str(i),charImg)
+
+                    threshPlate = cv2.adaptiveThreshold(charImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,13,5)
+                    cv2.imshow('Thresh: '+ str(i), threshPlate)
+
+                    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
+                    openImg = cv2.morphologyEx(threshPlate,cv2.MORPH_OPEN,kernel)
+
+
+                    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+                    #eroseImg = cv2.erode(threshPlate, kernel, iterations = 1)
+                    dilateImg = cv2.dilate(openImg, kernel, iterations = 1)'''
+                    
+                    old_x,old_y,old_w,old_h = x,y,w,h
+
+                    #cv2.imshow('Thresh char' + str(i), threshPlate)
+                    cv2.imshow('Dilate char' + str(i), charImg)
+                    char = image_to_string(charImg, config = config)
+                    if len(char) > 0:
+                        char = char[0]
+                    char = char.replace(" ", "")
+                    print(char,' ',x)
+                    plateNumber += char
+                print(plateNumber)
+                
+                
+                #print(plate_text)
+                return plateNumber
+
+        print("None")
+        return "None"
+
+
+
+# ==================================NOT SEPARATING CHARACTER===========================================
+# Convert to gray image
+'''grayPlate = cv2.cvtColor(found_plate, cv2.COLOR_BGR2GRAY)
+height,width = grayPlate.shape
+# increasing size of the image
+grayPlate = cv2.resize(grayPlate,(2*width,2*height))
+cv2.imshow('Gray', grayPlate)
+# Blur image
+blurPlate = cv2.GaussianBlur(grayPlate, (5, 5), 1.2) # Remove noise
+# Apply closing operation:  Dilation followed by Erosion to remove noise and hole inside object
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+closingImg = cv2.morphologyEx(blurPlate, cv2.MORPH_CLOSE, kernel)
+cv2.imshow('morpho',closingImg)
+# Apply threshold to get image with only b&w (binarization)
+#_, threshPlate = cv2.threshold(closingImg, 120, 255, cv2.THRESH_BINARY) #+ cv2.THRESH_OTSU)
+threshPlate = cv2.adaptiveThreshold(closingImg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,7,5)
+#v2.imshow('Thresh',threshPlate)
+# Store image in PIL image format
+pilImg = Image.fromarray(threshPlate)
+pilImg = np.array(pilImg)
+cv2.imshow('pilImg',pilImg)
+cv2.imwrite('test1.jpg',pilImg)
+# Recognize text with Tesseract
+plate_text = image_to_string(pilImg, lang = "eng")
+plate_text = plate_text.replace(" ", "") # Remove space
+
+if len(plate_text) == 0:
+    plate_text = "None"
+                    
+# Print Recognize text on image
+copy_img = np.copy(img)
+# Specify font
+font = cv2.FONT_HERSHEY_SIMPLEX
+# draw text
+cv2.putText(copy_img, plate_text, (x - w, y + 4*h), font, 2, (0,0,255), 2, cv2.LINE_AA)
+# Draw contour
+cv2.drawContours(copy_img, [cnt], 0, (0, 255, 0), 2)
+
+plt.figure()
+plt.imshow(cv2.cvtColor(copy_img, cv2.COLOR_BGR2RGB))
+plt.title("Plate number recognition")
+plt.axis("off")'''
