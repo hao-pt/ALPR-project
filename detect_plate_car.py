1899f67e899da5d2ead2a778de4c9ce6579d189f
tienhaophung <tienhaophung@gmail.com>
2019-06-03 00:10:15 +0700

    Bo so SVM

--- detect_plate_car.py
+++ detect_plate_car.py
@@ -3,11 +3,78 @@ import numpy as np
 from PIL import Image, ImageFilter
 from pytesseract import image_to_string
 import os
+import xml.etree.ElementTree as ET
+
+def classify_plate(plate_img):
+    tree = ET.parse(r'E:\K16\Junior\TGMT\ALPR-project\SVM.xml')  
+    root = tree.getroot()
+
+    # Read model parametrers and labels
+    SVM_TrainingData = root.find('TrainingData')
+    SVM_Classes = root.find('classes')
+    
+    training_data = SVM_TrainingData.find("data").text
+    labels = SVM_Classes.find("data").text
+
+    labels = labels.split(sep = " ")
+    rows = []
+    for c in labels:
+        if c == '\n' or c == '':
+            continue
+        if len(c) >= 2:
+            if '\n' in c:
+                num = int(c[:-1])  
+        else:
+            num = int(c)
+            
+        rows.append(num)
+
+    labels = np.array(rows, dtype = np.int32).reshape(-1, 1)
+
+    training_data = training_data.split(" ")
+    rows2 = []
+    for c in training_data:
+        if c == '\n' or c == '':
+            continue
+        if '\n' in c:
+            num = float(c[:-1])
+        else:
+            num = float(c)
+            
+        rows2.append(num)
+    
+    h, w = int(SVM_TrainingData.find("rows").text), int(SVM_TrainingData.find("cols").text)
+    training_data = np.array(rows2, dtype = np.float32).reshape((h, w))
+
+    # Set SVM params
+    svm = cv2.ml.SVM_create()
+    svm.setType(cv2.ml.SVM_C_SVC)
+    svm.setKernel(cv2.ml.SVM_LINEAR)
+    svm.setTermCriteria((cv2.TERM_CRITERIA_MAX_ITER, 1000, 0.01))
+    svm.setDegree(0)
+    svm.setGamma(1)
+    svm.setCoef0(0)
+    svm.setC(1)
+    svm.setNu(0)
+    svm.setP(0)
+    
+    print(training_data.shape, labels.shape)
+    # Train model
+    svm.train(training_data, cv2.ml.ROW_SAMPLE, labels)
+
+    # Predict
+    p = plate_img.reshape(1, -1).astype(np.float32)
+
+    response = svm.predict(p)[1]
+
+    if response == 1:
+        return True
+    return False
+
+
 
 # Load cascade model
 cascade = cv2.CascadeClassifier(r"E:\K16\Junior\TGMT\ALPR-project\haarcascade_russian_plate_number.xml") # haarcascade_licence_plate_rus_16stages.xml
-
-
 
 # # Test all file in dir
 # path = "E:\\K16\\Junior\\TGMT\\ALPR-project\\test_images\\"
@@ -76,8 +143,11 @@ for (x, y, w, h) in box_plates:
     # Plate roi
     plate_roi = np.copy(img[y:y+h, x:x + w])
     cv2.imshow("Plate ROI", plate_roi)
+
     # OCR
     # character_recognition(plate_roi, (x, y, w, h), img)
+
+# classify_plate()
 
 cv2.imshow("Plate detection", img)
 cv2.waitKey(0)
